cbd99ddde940aaf03d9990c5bf38a1a0e15d0337
DanielHenry <daniel.henry.39@gmail.com>
2019-03-11 00:36:08 +0700

    Make initial test for BillsController

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe BillsController, type: :controller do
-
+    context 'blank test' do
+        it 'is guaranteed to be success.' do
+            bills_controller = BillsController.new
+        end
+    end
 end
