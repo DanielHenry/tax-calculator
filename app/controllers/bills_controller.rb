69469a81c3d2ff1d9688bf02063aeeb91b59376c
DanielHenry <daniel.henry.39@gmail.com>
2019-03-13 06:04:50 +0700

    Initialize API 'GET /bills' and 'POST /bills'

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,2 +1,15 @@
 class BillsController < ApplicationController
+  def index
+    render json: [
+      {
+        name: 'Big Mac',
+        taxCode: 1,
+        price: 1000
+      }
+    ], status: :ok  
+  end
+
+  def create
+    head :created
+  end
 end
