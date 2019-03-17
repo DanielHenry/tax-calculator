3cfe3d91751d91e375a42837fec3df8e10805235
DanielHenry <daniel.henry.39@gmail.com>
2019-03-18 06:21:29 +0700

    Initiate index and create method in bills_controller

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,15 +1,28 @@
 class BillsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
-    render json: [
-      {
-        name: 'Big Mac',
-        taxCode: 1,
-        price: 1000
-      }
-    ], status: :ok  
+    bills = []
+    bill = {
+      'name' => 'Big Mac',
+      'taxCode' => 1,
+      'price' => 1000,
+    }
+    bills << bill
+    bill = {
+      'name' => 'Movie',
+      'taxCode' => 3,
+      'price' => 500,
+    }
+    bills << bill
+    render json: bills, status: :ok  
   end
 
   def create
     head :created
+  end
+
+  def json_request?
+    request.format.json?
   end
 end
