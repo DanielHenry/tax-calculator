3b61f3731d231325e208662380632cfb72584fdd
DanielHenry <daniel.henry.39@gmail.com>
2019-03-18 06:19:39 +0700

    Add several tests for bills_controller

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -1,9 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe BillsController, type: :controller do
-    context 'blank test' do
-        it 'is guaranteed to be success.' do
-            bills_controller = BillsController.new
-        end
+  context 'blank test' do
+    it 'is guaranteed to be success.' do
+      bills_controller = BillsController.new
     end
+  end
+
+  context 'when inserts 2 bills and get bills list' do
+    it 'success get 2 bills' do
+      post :create, :params => {
+        :format => 'json', 
+        :application => { 
+          :name => "Big Mac",
+          :taxCode => 1,
+          :price => 1000, 
+        } 
+      }
+      expect(response).to have_http_status(:created)      
+      post :create, :params => {
+        :format => 'json', 
+        :application => { 
+          :name => "Movie",
+          :taxCode => 3,
+          :price => 500, 
+        } 
+      }
+      expect(response).to have_http_status(:created)
+
+      get :index
+      expected = [
+        {
+          "name" => "Big Mac",
+          "taxCode" => 1,
+          "price" => 1000,
+        },
+        {
+          "name" => "Movie",
+          "taxCode" => 3,
+          "price" => 500,
+        }
+      ].to_json
+      expect(response.content_type).to eq("application/json")
+      expect(response.body).to eq expected
+    end
+  end
 end
